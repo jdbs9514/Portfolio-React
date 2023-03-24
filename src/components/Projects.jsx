36047a01454cba47169d38a7cfc385ffb1f5b44a
Josue Benavides <jdbs9514@gmail.com>
2023-03-24 00:58:43 -0500

    style projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 import '../style/Projects.css';
 import PP from '../assets/PP.jpg';
 
@@ -6,15 +7,28 @@ function Projects() {
   return (
     <section>
       <div className="projects">
-        <div className="title">
+        <div className="div-img">
           <img src={PP} alt="project 1" className="photo-project" />
         </div>
-        <div className="cards">
-          <h2>Lorem, ipsum.</h2>
-          <h4>Lorem.</h4>
-          <h4>Lorem.</h4>
-          <h3>View Project</h3>
-          <h3>View Code</h3>
+        <div className="div-text">
+          <h2 className="project-name">Lorem, ipsum.</h2>
+          <div className="skill-container">
+            <ul className="skills">
+              <li>Html</li>
+              <li>Css</li>
+              <li>Javascript</li>
+              <li>React</li>
+            </ul>
+          </div>
+          <p className="text-project">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Vitae aliquid quod expedita tenetur quia, id quas quaerat quam
+            repellat ducimus nisi blanditiis, explicabo reprehenderit, sit
+            i distinctio officiis ullam fugiat quis. Et vel quod officiis suscipit,
+          </p>
+        </div>
+        <div>
+          {/* <span><Link to="/">See Project</Link></span> */}
         </div>
       </div>
     </section>
