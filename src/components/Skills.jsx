6e1365245c7505b65d7e5afc18e36d754ea834c8
Josue Benavides <jdbs9514@gmail.com>
2023-05-25 17:58:41 -0500

    finish skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/Skills.css';
 import html from '../assets/html.png';
 import css from '../assets/css.png';
 import js from '../assets/js.png';
@@ -7,6 +8,7 @@ import webpack from '../assets/webpack.png';
 import postgres from '../assets/postgres.png';
 import ruby from '../assets/ruby.png';
 import rails from '../assets/rails.png';
+import bootstrap from '../assets/bootstrap.png';
 
 function Skills() {
   const skillArr = [
@@ -18,14 +20,18 @@ function Skills() {
     { id: 6, skill: postgres },
     { id: 7, skill: ruby },
     { id: 8, skill: rails },
+    { id: 9, skill: bootstrap },
   ];
 
   return (
-    <section>
-      <div className="skills-container">
+    <section className="skills-container">
+      <div className="skill-container-title">
+        <h2 className="skills-title">Skills</h2>
+      </div>
+      <div className="skill-item">
         {skillArr.map((item) => (
-          <div key={item.id} className="skill-item">
-            <img className="skills" src={item.skill} alt="skills" />
+          <div key={item.id} className="div-skill">
+            <img className="skill" src={item.skill} alt="skills" />
           </div>
         ))}
       </div>
